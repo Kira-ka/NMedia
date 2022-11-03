6735c40e1deb6300ab160aacf769f05eb7618e4e
Konovalov Kirill <kirkonowalow@gmail.com>
2022-11-03 13:06:09 +0300

    start Event

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,31 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            1,
+            "Нетология. Университет интернет-профессий будущего",
+            "21 мая в 18:36",
+            "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            false
+        )
+
+        with(binding) {
+            autor.text = post.author
+            time.text = post.published
+            content.text = post.content
+        }
+
+        binding.liks.setOnClickListener {
+            binding.liks.setImageResource(R.drawable.ic_baseline_favorite_24)
+        }
     }
 }
