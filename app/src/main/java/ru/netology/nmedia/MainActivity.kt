0d72b3f7549c2f4e736b6ff551b84b6990a31c6c
Konovalov Kirill <kirkonowalow@gmail.com>
2022-11-14 12:35:03 +0300

    RecyclerView with ListAdapter.

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         })
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.list = posts
+            adapter.submitList(posts)
         }
 
     }
